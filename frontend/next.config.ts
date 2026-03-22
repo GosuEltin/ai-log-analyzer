ce81c82b1ccf0ef8b6f2ab6d0ed9b9d34d67bd86
WHT413 <haryqvs@outlook.com>
2026-03-22 14:55:00 +0700

    feat: Initialize project structure with Next.js frontend, Python configuration, and initial test results.

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
+const allowedOrigins = ["127.0.0.1", "localhost"];
+if (process.env.TAILSCALE_IP) {
+  allowedOrigins.push(process.env.TAILSCALE_IP);
+}
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  allowedDevOrigins: allowedOrigins,
 };
 
 export default nextConfig;
